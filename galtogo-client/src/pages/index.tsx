a9e27aa50e66d1298bd565846070bbf3c1b5daf2
Morning <morning@1-14.local>
2023-04-10 11:41:33 +0800

    Eslint working fine:) & Tailwind works

--- galtogo-client/src/pages/index.tsx
+++ galtogo-client/src/pages/index.tsx
@@ -1,6 +1,21 @@
-
 export default function Home() {
-  return <div>
-    <h1>Test</h1>
-  </div>;
+  // function testHandler() {}
+  return (
+    <div>
+      <ul className="flex gap-2 text-blue-600 text-xl p-5">
+        <li className="px-6 py-2 border border-sky-600 rounded-full hover:bg-blue-600 hover:text-white transition">
+          Home
+        </li>
+        <li className="px-6 py-2 border border-sky-600 rounded-full hover:bg-blue-600 hover:text-white transition">
+          Order
+        </li>
+        <li className="px-6 py-2 border border-sky-600 rounded-full hover:bg-blue-600 hover:text-white transition">
+          About us
+        </li>
+        <li className="px-6 py-2 border border-sky-600 rounded-full hover:bg-blue-600 hover:text-white transition">
+          Contact
+        </li>
+      </ul>
+    </div>
+  );
 }
