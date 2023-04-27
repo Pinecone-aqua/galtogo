b41ee97174648d5978b9f22f6eb94cae8aa4a8e0
Tugarik <tugarik@gmail.com>
2023-04-28 02:10:33 +0800

    client side props transfer made

--- galtogo-client/src/components/MenuField.tsx
+++ galtogo-client/src/components/MenuField.tsx
@@ -1,23 +1,11 @@
 import MenuCard from "./subcomponents/MenuCard";
 
-const items = [
-  { name: "Item-1", category: "Category-1", price: "ItemPrice-1-1" },
-  { name: "Item-2", category: "Category-1", price: "ItemPrice-2-1" },
-  { name: "Item-3", category: "Category-1", price: "ItemPrice-3-1" },
-  { name: "Item-4", category: "Category-1", price: "ItemPrice-4-1" },
-  { name: "Item-5", category: "Category-1", price: "ItemPrice-5-1" },
-  { name: "Item-1", category: "Category-2", price: "ItemPrice-1-2" },
-  { name: "Item-2", category: "Category-2", price: "ItemPrice-2-2" },
-  { name: "Item-3", category: "Category-2", price: "ItemPrice-3-2" },
-  { name: "Item-4", category: "Category-2", price: "ItemPrice-4-2" },
-  { name: "Item-5", category: "Category-2", price: "ItemPrice-5-2" },
-];
-// const product = { name: "Item-1", category: "Category-1" };
-
 export default function MenuField({
   selectedCategory,
+  items,
 }: {
   selectedCategory: string;
+  items: IProduct[];
 }) {
   return (
     <div className="flex flex-wrap justify-center">
--- galtogo-client/src/pages/index.tsx
+++ galtogo-client/src/pages/index.tsx
@@ -4,6 +4,19 @@ import MenuField from "@/components/MenuField";
 import SpecialOffer from "@/components/SpecialOffer";
 import { useState } from "react";
 
+const items = [
+  { name: "Item-1", category: "Category-1", price: "ItemPrice-1-1" },
+  { name: "Item-2", category: "Category-1", price: "ItemPrice-2-1" },
+  { name: "Item-3", category: "Category-1", price: "ItemPrice-3-1" },
+  { name: "Item-4", category: "Category-1", price: "ItemPrice-4-1" },
+  { name: "Item-5", category: "Category-1", price: "ItemPrice-5-1" },
+  { name: "Item-1", category: "Category-2", price: "ItemPrice-1-2" },
+  { name: "Item-2", category: "Category-2", price: "ItemPrice-2-2" },
+  { name: "Item-3", category: "Category-2", price: "ItemPrice-3-2" },
+  { name: "Item-4", category: "Category-2", price: "ItemPrice-4-2" },
+  { name: "Item-5", category: "Category-2", price: "ItemPrice-5-2" },
+];
+
 export default function Home(): JSX.Element {
   const [selectedCategory, setSelectedCategory] =
     useState<string>("Category-1");
@@ -16,7 +29,7 @@ export default function Home(): JSX.Element {
         <Carousel setSelectedCategory={setSelectedCategory} />
       </div>
       <div className="p-3">
-        <MenuField selectedCategory={selectedCategory} />
+        <MenuField selectedCategory={selectedCategory} items={items} />
       </div>
       <div className="p-3">Additional information</div>
     </Layout>
