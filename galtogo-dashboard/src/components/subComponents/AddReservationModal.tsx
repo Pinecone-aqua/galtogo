611a1df2f879d365cfe0ee2aeaa9ec41058d8ace
Tugarik <tugarik@gmail.com>
2023-05-03 23:18:24 +0800

    Add reservation by phone number

--- galtogo-dashboard/src/components/subComponents/AddReservationModal.tsx
+++ galtogo-dashboard/src/components/subComponents/AddReservationModal.tsx
@@ -1,22 +1,44 @@
+import { tableTimes } from "@/utils/constants";
 import axios from "axios";
+import moment from "moment";
+import { toast } from "react-toastify";
 
-export default function AddReservationModal({ setShowAddModal }) {
+export default function AddReservationModal({
+  setShowAddModal,
+  tablesData,
+}: {
+  setShowAddModal: any;
+  tablesData: ITable[];
+}): JSX.Element {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async function handleSubmit(e: any) {
     e.preventDefault();
+    const userId = await axios
+      .get(`http://localhost:5050/user/${e.target.phone.value}`)
+      .then((res) => res.data)
+      .catch((err) => console.log("user: ", err));
+
     const newReservation = {
       time: e.target.time.value,
       date: e.target.date.value,
       persons: e.target.persons.value,
-      user: e.target.user.value,
+      user: userId._id,
       table: e.target.table.value,
       status: "pending",
     };
-    setShowAddModal((prev) => !prev);
+    setShowAddModal((prev: boolean) => !prev);
     try {
       await axios
         .post("http://localhost:5050/reservation/add", newReservation)
-        .then((res) => console.log(res));
+        .then((res) => {
+          console.log(res);
+          toast.success("Reservation successfully added!");
+        })
+        .catch((err) =>
+          toast.error(`Wrong phone number! ${err.message}`, {
+            position: "top-center",
+          })
+        );
     } catch (error) {
       console.log(error);
     }
@@ -26,35 +48,42 @@ export default function AddReservationModal({ setShowAddModal }) {
       className="absolute top-20 left-0 bg-slate-200 p-3 flex flex-col z-10 rounded-xl"
       onSubmit={handleSubmit}
     >
+      <label>Date:</label>
       <input
         className="m-2 p-2 rounded-xl"
         type="text"
         name="date"
-        defaultValue="2023-04-29"
+        defaultValue={moment().format("YYYY-MM-DD")}
       />
-      <input
-        className="m-2 p-2 rounded-xl"
-        type="text"
-        name="time"
-        defaultValue="18:00"
-      />
-      <input
-        className="m-2 p-2 rounded-xl"
-        type="text"
-        name="table"
-        defaultValue="644083ce00cd4c337032f46d"
-      />
+      <label>Time:</label>
+      <select className="m-2 p-2 rounded-xl" name="time">
+        {tableTimes.map((times, index) => (
+          <option key={index} value={times.time}>
+            {times.time}
+          </option>
+        ))}
+      </select>
+      <label>Table:</label>
+      <select className="m-2 p-2 rounded-xl" name="table">
+        {tablesData.map((table, index) => (
+          <option key={index} value={table._id}>
+            {`Table # ${table.name}`}
+          </option>
+        ))}
+      </select>
+      <label>Persons:</label>
       <input
         className="m-2 p-2 rounded-xl"
         type="number"
         name="persons"
         defaultValue={6}
       />
+      <label>Phone:</label>
       <input
         className="m-2 p-2 rounded-xl"
-        type="text"
-        name="user"
-        defaultValue="643cc79f35de8088d1b1b08d"
+        type="string"
+        name="phone"
+        placeholder="88112233"
       />
       <input
         type="submit"
