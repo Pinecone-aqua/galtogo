bf4fb34283da4dc04639dcb5ec133a9d877c46da
Nominest <114572342+Nominest@users.noreply.github.com>
2023-05-10 23:08:42 +0800

    added load subcomponent

--- galtogo-dashboard/src/pages/_app.tsx
+++ galtogo-dashboard/src/pages/_app.tsx
@@ -1,51 +1,16 @@
 import "@/styles/globals.css";
-//theme
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-
-//core
 import "primereact/resources/primereact.min.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
-function Loading(): false | JSX.Element {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const handleStart = (url: string) =>
-      url !== router.asPath && setLoading(true);
-    const handleComplete = (url: string) =>
-      url === router.asPath &&
-      setTimeout(() => {
-        setLoading(false);
-      }, 2000);
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  });
-  return (
-    loading && (
-      <div className="">
-        <div className="" />
-      </div>
-    )
-  );
-}
+import Load from "@/components/subComponents/Load";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Loading />
+      <Load />
       <Component {...pageProps} />
     </>
   );
 }
+
 export default App;
