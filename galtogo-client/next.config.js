a5df0696738ae11746b1261074e5b3d8a4b77f70
Morning <morning@1-14.local>
2023-05-11 13:22:49 +0800

    menu field

--- galtogo-client/next.config.js
+++ galtogo-client/next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/**@type {import('next').NextConfig} */ const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
