d6768cf728a7d4c7a7aa0e468c0a420ddc35cb22
Morning <morning@1-14.local>
2023-05-11 13:09:50 +0800

    footer responsive

--- galtogo-client/src/components/Footer.tsx
+++ galtogo-client/src/components/Footer.tsx
@@ -1,43 +1,43 @@
-import { socialIcons } from "@/utils/constants";
-import { TbChefHat } from 'react-icons/tb'
+import { footerMenuItems } from "@/utils/constants"
+import {
+  FaFacebook,
+  FaFacebookMessenger,
+  FaTwitter,
+  FaInstagram,
+  FaGithub,
+} from "react-icons/fa";
 
 export default function Footer() {
-  return <div><div className="flex justify-between gap-10 w-full p-10 mt-20">
-    <div className="w-1/4">
-      <h1 className="font-bold p-2 text-lg text-orange-500">Байршил</h1>
-      <p className="p-2 text-gray-400 w-[250px]">Олимпын гудамж, 1-р хороо, Сүхбаатар дүүрэг, Улаанбаатар хот, Монгол улс NewHorizon center - 401 тоот</p>
-    </div>
+  return (
+    <div className="px-[40px]">
+      <div className="flex flex-col sm:justify-between sm:items-center md:items-center md:justify-between sm:flex-row py-4">
 
-    <div className="w-1/4">
-      <h1 className="p-2 text-lg font-bold text-orange-500">Цагийн хуваарь</h1>
-      <p className="p-2 text-gray-400">11:00 - 18:00 (ажлын өдөр)</p>
-      <p className="p-2 text-gray-400">11:00 - 17:00 (амралтын өдөр)</p>
-    </div>
-
-    <div className="w-1/4  ">
-      <h1 className="p-2 text-lg font-bold text-orange-500">Сошиал сувгууд</h1>
-      <div className="flex gap-2 p-2">
-        {socialIcons.map((item, index) => <div className="text-gray-400" key={index}>{item.icon}</div>)}
+        <div>
+          <picture>
+            <img className="w-[72px] mb-4 sm:mb-0" src="./logo.png" alt="logo" />
+          </picture>
+        </div>
+        <div className="border sm:hidden md:hidden" />
+        <div className="flex flex-col sm:flex-row">
+          {footerMenuItems.map((item: { name: string, icon: JSX.Element }, index: number) => (
+            <div className="flex items-center text-sm gap-1 justify-between  p-2 md:text-base lg:text-base" key={index}>
+              <div>{item.icon}</div>
+              <div className="w-full">{item.name}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="border" />
+      <div className="flex flex-col sm:justify-between py-8 sm:items-center sm:flex-row ">
+        <p>© 2023 Developed by Galtogo Team</p>
+        <div className="flex gap-2 text-slate-300 mt-4 sm:mt-0 md:mt-0">
+          <FaFacebook size={'24px'} />
+          <FaFacebookMessenger size={'24px'} />
+          <FaTwitter size={'24px'} />
+          <FaInstagram size={'24px'} />
+          <FaGithub size={'24px'} />
+        </div>
       </div>
     </div>
-
-    <div className="w-1/4 ">
-      <h1 className="p-2 font-bold text-lg text-orange-500">Холбоо барих</h1>
-      <p className="p-2 text-gray-400">order@galtogo.mn</p>
-      <p className="p-2 text-gray-400">hire@galtogo.mn</p>
-      <p className="p-2 text-gray-400">Nexus@galtogo.mn</p>
-    </div>
-
-
-
-  </div>
-    <div className="flex justify-between items-center">
-      <div className="flex items-center gap-3 p-4">
-        <TbChefHat />
-
-        <div className="p-4 text-gray-500">© 2017-2023 Зохиогчийн эрх хамгаалагдсан</div>
-      </div>
-      <div className="text-gray-500">Powered by Galtogo team</div>
-    </div>
-  </div>
+  );
 }
