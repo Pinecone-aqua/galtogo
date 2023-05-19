77af0902d793aa6ff1710dda85337456e864d272
Tugarik <tugarik@gmail.com>
2023-05-19 13:01:54 +0800

    jwt started

--- galtogo-server/src/user/user.controller.ts
+++ galtogo-server/src/user/user.controller.ts
@@ -3,27 +3,37 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
+  Query,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { IUser } from './user.schema';
 import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response } from 'express';
 
 @Controller('user')
 export class UsersController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Get()
   getUser(): Promise<IUser[]> {
     return this.userService.getUser();
   }
 
-  @Get(':phone')
+  @Get('phone/:phone')
   getUserByPhone(@Param('phone') phone: string): Promise<IUser> {
-    return this.userService.getUserByPhone(Number(phone));
+    return this.userService.getUserByPhone(phone);
   }
 
   @Post('add')
@@ -40,4 +50,34 @@ export class UsersController {
   deleteUser(@Param('id') id: string) {
     return this.userService.deleteUser(id);
   }
+
+  // @Get('auth/user')
+  // async verifyUser(
+  //   @Query() query: { id: string },
+  //   @Res() res: Response,
+  //   // @Req() req: Request,
+  // ) {
+  //   console.log('auth request revieved');
+  //   console.log('auth:', query.id);
+  //   const id = query.id;
+  //   if (!id) throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+
+  //   const user = await this.userService.getUserById(query.id);
+  //   console.log('user: ', user);
+
+  //   const payload = {
+  //     firstName: user.firstName,
+  //     email: user.email,
+  //     role: user.role,
+  //     phone: user.phone,
+  //   };
+  //   console.log('payload: ', payload);
+
+  //   const token = this.jwtService.sign(payload);
+  //   console.log('token: ', token);
+  //   res
+  //     .status(200)
+  //     .cookie('token', token)
+  //     .redirect(`https://galtogo.vercel.app/account`);
+  // }
 }
