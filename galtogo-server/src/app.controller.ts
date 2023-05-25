c9605cdc26a0d83f8fb7faf54f4df046b754e5ac
Tugarik <tugarik@gmail.com>
2023-05-25 18:24:39 +0800

    fileStream

--- galtogo-server/src/app.controller.ts
+++ galtogo-server/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('.well-known/pki-validation')
+  getFile(): StreamableFile {
+    const file = createReadStream(
+      join(process.cwd(), '999550D5A343324934EF6361B33DDB04.txt'),
+    );
+    return new StreamableFile(file);
   }
 }
