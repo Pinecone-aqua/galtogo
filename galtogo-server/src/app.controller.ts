0f693b9309eec6613cc4293bf5c84750c4568db4
Tugarik <tugarik@gmail.com>
2023-05-25 18:42:58 +0800

    ssl file stream

--- galtogo-server/src/app.controller.ts
+++ galtogo-server/src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Res, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
 import { createReadStream } from 'fs';
 import { join } from 'path';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -12,11 +13,16 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('.well-known/pki-validation')
-  getFile(): StreamableFile {
+  @Get('.well-known/pki-validation/999550D5A343324934EF6361B33DDB04.txt')
+  getFile(@Res({ passthrough: true }) res: Response): StreamableFile {
     const file = createReadStream(
       join(process.cwd(), '999550D5A343324934EF6361B33DDB04.txt'),
     );
+    res.set({
+      'Content-Type': 'application/json',
+      'Content-Disposition':
+        'attachment; filename="999550D5A343324934EF6361B33DDB04.txt"',
+    });
     return new StreamableFile(file);
   }
 }
